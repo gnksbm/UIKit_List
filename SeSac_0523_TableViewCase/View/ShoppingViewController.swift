311e43ad20cf21171e39900f2fd6ccddc338b782
Geonseob Kim <gnksbm@gmail.com>
2024-05-23 22:06:08 +0900

    ShoppingViewController 구현

---
+++ SeSac_0523_TableViewCase/View/ShoppingViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ShoppingViewController.swift
+//  SeSac_0523_TableViewCase
+//
+//  Created by gnksbm on 5/23/24.
+//
+
+import UIKit
+
+final class ShoppingViewController: UITableViewController {
+    @UserDefaultsWrapper(key: "shoppingList", defaultValue: [])
+    private var shoppingList: [ShoppingModel]
+    
+    @IBOutlet var textFieldBackgroundView: UIView!
+    @IBOutlet var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTextField()
+        configureTableView()
+    }
+    
+    private func configureTextField() {
+        textFieldBackgroundView.layer.cornerRadius = 10
+    }
+    
+    private func configureTableView() {
+        tableView.register(
+            ShoppingTableViewCell.self,
+            forCellReuseIdentifier: String(describing: ShoppingTableViewCell.self)
+        )
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        do {
+            try shoppingList.addNewItem(message: textField.text)
+            tableView.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension ShoppingViewController {
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        shoppingList.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: ShoppingTableViewCell.self),
+            for: indexPath
+        ) as? ShoppingTableViewCell else { return .init() }
+        cell.updateUI(model: shoppingList[indexPath.row])
+        cell.setCheckButtonClosure {
+            self.shoppingList.togglePurchased(index: indexPath.row)
+            tableView.reloadData()
+        }
+        cell.setFavoriteButtonClosure {
+            self.shoppingList.toggleFavorite(index: indexPath.row)
+            tableView.reloadData()
+        }
+        return cell
+    }
+}
